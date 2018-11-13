c4b40dd73702daef02f8cc849d2e5a6ba4bdf00b
xianglijun <275923192@qq.com>
2018-11-13 18:41:30 +0800

    first commit

---
+++ Assets/Scripts/CoreBase/BaseUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseUI : MonoBehaviour {
+
+    public string uiPath { get; private set; }
+    public string uiName { get; private set; }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Ace
+{
+    public static class Singleton<T> where T : class
+    {
+        private static T _instance;
+        static Singleton()
+        {
+            return;
+        }
+        public static void Create()
+        {
+            _instance = (T)Activator.CreateInstance(typeof(T), true);
+            return;
+        }
+        public static T Instance
+        {
+            get
+            {
+               return _instance;
+            }
+        }
+        public static void Destory()
+        {
+            _instance = null;
+            return;
+        }
+    }
+}
